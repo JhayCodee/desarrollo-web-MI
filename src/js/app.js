746d02511bd35c3a490b041e4637ce821d6392fd
Luis Vargas <kluis0178@gmail.com>
2022-03-02 21:08:43 -0600

    header - navbar done

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const navlinks = document.querySelectorAll('.nav-link');
+
+Menu();
+
+function Menu() {
+
+    navToggle.addEventListener('click', () => {
+        toggleMenu();
+    });
+
+    navlinks.forEach( (link) => {
+        link.addEventListener('click', () => {
+            toggleMenu();
+        });
+    });
+    
+}
+
+function toggleMenu() {
+    navMenu.classList.toggle('nav-menu_visible');
+}
+
+
