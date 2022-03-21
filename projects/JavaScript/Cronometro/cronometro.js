0cb7c4ed86a63aabb82ddf846676ce3a56194792
Luis Vargas <kluis0178@gmail.com>
2022-03-20 22:07:42 -0600

    cronometro terminado

--- projects/JavaScript/Cronometro/cronometro.js
+++ projects/JavaScript/Cronometro/cronometro.js
@@ -1,4 +1,4 @@
-const time = document.querySelector('.tiempo');
+const time = document.querySelector('.contenedor .tiempo');
 
 const btnStart = document.getElementById('startTimer');
 const btnPause = document.getElementById("pauseTimer");
@@ -6,3 +6,46 @@ const btnReset = document.getElementById('resetTimer');
 
 let seconds = 0;
 let interval = null;
+
+
+btnStart.addEventListener('click', start);
+btnPause.addEventListener('click', stop);
+btnReset.addEventListener('click', reset);
+
+
+function timer () {
+
+    seconds++;
+
+    // fromatear tiempo
+    let hours = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - (hours * 3600)) / 60);
+    let secs = seconds % 60;
+
+    if (secs < 10) secs = '0' + secs;
+    if (mins < 10) mins = '0' + mins;
+    if (hours < 10) hours = '0' + hours;
+ 
+
+    time.innerHTML = `${hours}:${mins}:${secs}`;
+}
+
+
+function start() {
+    
+    if (interval) return
+
+    interval = setInterval(timer, 1000)
+
+}
+
+function stop () {
+    clearInterval(interval);
+    interval = null;
+}
+
+function reset () {
+    stop();
+    seconds = 0;
+    time.innerHTML = '00 : 00 : 00'
+}
