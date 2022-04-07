7cfe24873d7d71fffb50f32dc92021e6069f92a8
Luis Vargas <kluis0178@gmail.com>
2022-04-06 22:46:20 -0600

    animacion inicio

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,32 @@
+const texts = ['Estudio Ingeniería', 'Desarollador Web'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+
+    if(count === texts.length) count = 0;
+
+    currentText = texts[count]
+    letter = currentText.slice(0, ++index);
+    document.querySelector('.typing').textContent = letter;
+
+    if (letter.length === currentText.length){
+        count++;
+        index = 0;
+    }  
+
+    setTimeout(type, 400);
+    
+}());
+
+
+
+/*
 const navlinks = document.querySelectorAll('.nav-link');
 
 Menu();
-
-
 
 function Menu() {
 
@@ -16,3 +40,4 @@ function Menu() {
     });
     
 }
+*/
